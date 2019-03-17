200c6e026d4abefec22463311c6f88eb86667f20
Tommi Hyvärinen <Tommi.Hyvarinen@edu.savonia.fi>
2019-03-17 15:19:33 +0200

    muutos ui:hin

--- sudoku/pelilauta.cs
+++ sudoku/pelilauta.cs
@@ -29,11 +29,62 @@ namespace sudoku
             }
         }
 
-        
+
 
         private void valitseNumero(object sender, EventArgs e)
         {
             Button nappi = (Button)sender;
+
+            int sarakeNro = tableLayoutPanel1.GetColumn(nappi);
+            int riviNro = tableLayoutPanel1.GetRow(nappi);
+
+            for (int i = 0; i < 9; i++)
+            {
+                if (i > 2 && i < 6)
+                {
+                    for (int k = 0; k < 3; k++)
+                    {
+                        tableLayoutPanel1.GetControlFromPosition(k, i).BackColor = SystemColors.ControlLightLight;
+                    }
+                    for (int k = 0; k < 3; k++)
+                    {
+                        tableLayoutPanel1.GetControlFromPosition(k+3, i).BackColor = Color.Gainsboro;
+                    }
+                    for (int k = 0; k < 3; k++)
+                    {
+                        tableLayoutPanel1.GetControlFromPosition(k+6, i).BackColor = SystemColors.ControlLightLight;
+                    }
+                }
+                else
+                {
+                    for (int k = 0; k < 3; k++)
+                    {
+                        Control c = tableLayoutPanel1.GetControlFromPosition(k, i);
+                        c.BackColor = Color.Gainsboro;
+                    }
+
+                    for (int k = 0; k < 3; k++)
+                    {
+                        tableLayoutPanel1.GetControlFromPosition(k+3, i).BackColor = SystemColors.ControlLightLight;
+                    }
+
+                    for (int k = 0; k < 3; k++)
+                    {
+                        tableLayoutPanel1.GetControlFromPosition(k+6, i).BackColor = Color.Gainsboro;
+                    }
+                }
+            }
+
+            for (int i = 0; i < tableLayoutPanel1.RowCount; i++)
+            {
+                tableLayoutPanel1.GetControlFromPosition(sarakeNro, i).BackColor = Color.Silver;
+            }
+            for (int i = 0; i < tableLayoutPanel1.ColumnCount; i++)
+            {
+                tableLayoutPanel1.GetControlFromPosition(i, riviNro).BackColor = Color.Silver;
+            }
+
+
 
             if (nappi.Text == "") // mikäli nappi on tyhjä, eli ei ole numeroa, niin avataan numerovalitsin
             {
